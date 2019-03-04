6ee27d32d71a8b9bb1955b0e4bbd0e87433b7132
m.abdullah <m.abdullah@Hassan-PC.csd.local>
2019-03-04 21:07:06 +0500

    yu

--- src/test/java/com/qa/test/User_Registration.java
+++ src/test/java/com/qa/test/User_Registration.java
@@ -60,7 +60,8 @@ public class User_Registration extends TestBase {
 	             .put("gender", "f")
 	        	 .put("phone", "+15142546014");
 		
-		JSONObject val = new JSONObject().put("value", "Germany0!");
+		JSONObject val = new JSONObject()
+				.put("value", "Germany0!");
 		
 		JSONObject passwordJson = new JSONObject().put("password", val);
 		
@@ -109,6 +110,79 @@ public class User_Registration extends TestBase {
 		System.out.println("Last Name is-->"+ last_name +"\n");
 		
 		Assert.assertEquals(last_name, "user");	      
+		
+	}
+	
+	
+	@Test(priority=2, enabled=false)
+	
+	public void Account_Already_Exist() throws ClientProtocolException, IOException
+	{
+		String token = getsession_obj.Create_Get_Session();
+		
+		System.out.println(token);
+		
+		user_register_url = prop.getProperty("user_register_url");
+		
+        HashMap<String, String> headerMap = new HashMap<String, String>();
+		
+		headerMap.put("Authorization", "Bearer "+token);
+		
+		headerMap.put("Accept", "application/json");
+		
+		headerMap.put("Content-Type", "application/json");
+		
+		headerMap.put("SSDEBUG", "SelfieStylerDev"); 
+		
+		JSONObject userJson = new JSONObject()
+	             .put("username","test00020@selfiestyler.com")
+	             .put("email","test00020@selfiestyler.com")
+	             .put("first_name", "test")
+	             .put("last_name", "user")
+	             .put("gender", "f")
+	        	 .put("phone", "+15142546014");
+		
+		JSONObject val = new JSONObject()
+				.put("value", "Germany0!");
+		
+		JSONObject passwordJson = new JSONObject().put("password", val);
+		
+	String testJson = new JSONObject()
+        .put("device_id", "1234567890123456789012345678901234567890")
+        .put("device_type", "iphone6")
+        .put("is_subscribed", false)
+        .put("api_version", "123")
+        .put("user", userJson)
+        .put("credentials", passwordJson).toString();  
+	   		
+	  System.out.println("\n  Request "+testJson+"\n");
+	   
+	   closebaleHttpResponse = restClient.post(user_register_url, testJson, headerMap); //call the API
+	   
+	  int statusCode = closebaleHttpResponse.getStatusLine().getStatusCode(); //validate response from API:
+	 	    
+	  System.out.println("Value of status code is: "+ statusCode +"\n"); 	//1. status code:
+	 	    
+	  Assert.assertEquals(statusCode, testBase.RESPONSE_STATUS_CODE_422);
+	   
+	 //2. JsonString:
+	   String responseString = EntityUtils.toString(closebaleHttpResponse.getEntity(), "UTF-8");
+	 				
+	   JSONObject responseJson = new JSONObject(responseString);
+	 		
+	    System.out.println("The response from API is:"+ responseJson +"\n");
+
+        String error_message = TestUtil.getValueByJPath(responseJson, "/error_message");
+		
+		System.out.println("Error message is-->-->"+ error_message +"\n");
+		
+		Assert.assertEquals(error_message, "An account already exists for this username. Please login or register with a different username.");
+		
+        String error_summary = TestUtil.getValueByJPath(responseJson, "/error_summary");
+		
+		System.out.println("Error summary is-->"+ error_summary +"\n");
+		
+		Assert.assertEquals(error_summary, "Account Already Exists");	      
 		
 	}
 
